5c00a281c572436b956b08dfc44eaea73de5a82c
Leon Do <91256597+Random-Palico@users.noreply.github.com>
2023-12-03 09:06:39 +0700

    update code
    scrolling working

--- tabletalk_mobile/lib/screens/recommendation_screen/recommendation_screen.dart
+++ tabletalk_mobile/lib/screens/recommendation_screen/recommendation_screen.dart
@@ -1,14 +1,56 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:tabletalk_mobile/core/app_export.dart';
-import 'package:tabletalk_mobile/widgets/custom_text_form_field.dart';
-import 'package:tabletalk_mobile/widgets/custom_top_bar.dart';
+// ignore_for_file: must_be_immutable
 
-// ignore: must_be_immutable
+import 'package:flutter/material.dart';
+import 'package:tabletalk_mobile/core/app_export.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tabletalk_mobile/widgets/custom_top_bar.dart';
+import 'package:tabletalk_mobile/widgets/custom_text_form_field.dart';
+import 'package:tabletalk_mobile/widgets/info_box.dart';
+
 class RecommendationScreen extends StatelessWidget {
   RecommendationScreen({Key? key}) : super(key: key);
 
   TextEditingController askController = TextEditingController();
+
+  // Customize the text and info boxes as needed
+  final String recipeText = "Recipe";
+  final String restaurantText = "Restaurant";
+
+  final List<InfoBox> customRecipeInfoBoxes = [
+    InfoBox(
+        title: 'Custom Recipe Info 1',
+        description: 'Custom Recipe Description 1'),
+    InfoBox(
+        title: 'Custom Recipe Info 2',
+        description: 'Custom Recipe Description 2'),
+    InfoBox(
+        title: 'Custom Recipe Info 2',
+        description: 'Custom Recipe Description 2'),
+    InfoBox(
+        title: 'Custom Recipe Info 2',
+        description: 'Custom Recipe Description 2'),
+    InfoBox(
+        title: 'Custom Recipe Info 2',
+        description: 'Custom Recipe Description 2'),
+    InfoBox(
+        title: 'Custom Recipe Info 2',
+        description: 'Custom Recipe Description 2'),
+    InfoBox(
+        title: 'Custom Recipe Info 2',
+        description: 'Custom Recipe Description 2'),
+
+    // ... Add more later
+  ];
+
+  final List<InfoBox> customRestaurantInfoBoxes = [
+    InfoBox(
+        title: 'Custom Restaurant Info 1',
+        description: 'Custom Restaurant Description 1'),
+    InfoBox(
+        title: 'Custom Restaurant Info 2',
+        description: 'Custom Restaurant Description 2'),
+    // ... Add more later
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -17,91 +59,104 @@ class RecommendationScreen extends StatelessWidget {
       extendBody: true,
       extendBodyBehindAppBar: true,
       body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          padding: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // First Row
-              Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        RichText(
-                          text: TextSpan(
-                            style: const TextStyle(
-                              color: Color.fromRGBO(253, 99, 124, 1),
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            children: [
-                              const TextSpan(
-                                text: 'Search Results... ',
+        child: SingleChildScrollView(
+          // Wrap your content with SingleChildScrollView
+          child: Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          RichText(
+                            text: TextSpan(
+                              style: const TextStyle(
+                                color: Color.fromRGBO(253, 99, 124, 1),
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
                               ),
-                              TextSpan(
-                                text: '50 results',
-                                style: const TextStyle(
-                                  color: Color.fromARGB(255, 194, 194, 194),
-                                  fontSize: 11,
+                              children: [
+                                const TextSpan(
+                                  text: 'Search Results... ',
                                 ),
-                              ),
-                            ],
+                                TextSpan(
+                                  text: '50 results',
+                                  style: const TextStyle(
+                                    color: Color.fromARGB(255, 194, 194, 194),
+                                    fontSize: 11,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          CustomTextFormField(
+                            controller: askController,
+                            hintText: "Ask us anything!",
+                            textInputAction: TextInputAction.done,
+                            textStyle: TextStyle(
+                              color: askController.text.isNotEmpty
+                                  ? const Color.fromARGB(255, 194, 194, 194)
+                                  : const Color.fromARGB(255, 0, 0, 0),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 0.0, left: 9),
+                          child: InkWell(
+                            onTap: () {
+                              //  bell icon click logic here
+                            },
+                            child: SvgPicture.asset(
+                              ImageConstant.imgBell,
+                              width: 42,
+                              height: 42,
+                            ),
                           ),
                         ),
-                        SizedBox(height: 8),
-                        CustomTextFormField(
-                          controller: askController,
-                          hintText: "Ask us anything!",
-                          textInputAction: TextInputAction.done,
-                          textStyle: TextStyle(
-                            color: askController.text.isNotEmpty
-                                ? const Color.fromARGB(255, 194, 194, 194)
-                                : const Color.fromARGB(255, 0, 0, 0),
+                        SizedBox(height: 0),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 5.0, left: 10),
+                          child: InkWell(
+                            onTap: () {
+                              // filter icon click logic here
+                            },
+                            child: SvgPicture.asset(
+                              ImageConstant.imgFilter,
+                              width: 50,
+                              height: 50,
+                            ),
                           ),
                         ),
                       ],
                     ),
-                  ),
-                  Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 0.0, left: 9),
-                        child: InkWell(
-                          onTap: () {
-                            //  bell icon click logic here
-                          },
-                          child: SvgPicture.asset(
-                            ImageConstant.imgBell,
-                            width: 42,
-                            height: 42,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 0),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 5.0, left: 10),
-                        child: InkWell(
-                          onTap: () {
-                            // filter icon click logic here
-                          },
-                          child: SvgPicture.asset(
-                            ImageConstant.imgFilter,
-                            width: 50,
-                            height: 50,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              // Second Row
-              CustomTopBar(),
-            ],
+                  ],
+                ),
+
+                SizedBox(height: 16),
+                // Second Row
+                CustomTopBar(
+                  onChanged: (type) {
+                    // Handle changes in RecommendationScreen
+                  },
+                  menuItems: [
+                    TopMenuModel(text: recipeText, type: TopBarEnum.Item1),
+                    TopMenuModel(text: restaurantText, type: TopBarEnum.Item2),
+                  ],
+                  recipeInfoBoxes: customRecipeInfoBoxes,
+                  restaurantInfoBoxes: customRestaurantInfoBoxes,
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- tabletalk_mobile/lib/widgets/custom_top_bar.dart
+++ tabletalk_mobile/lib/widgets/custom_top_bar.dart
@@ -1,13 +1,21 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
-import 'package:tabletalk_mobile/core/app_export.dart';
 import 'info_box.dart';
 
 class CustomTopBar extends StatefulWidget {
-  CustomTopBar({super.key, this.onChanged});
+  CustomTopBar({
+    Key? key,
+    required this.onChanged,
+    required this.menuItems,
+    required this.recipeInfoBoxes,
+    required this.restaurantInfoBoxes,
+  }) : super(key: key);
 
-  Function(TopBarEnum)? onChanged;
+  final Function(dynamic) onChanged;
+  final List<TopMenuModel> menuItems;
+  final List<InfoBox> recipeInfoBoxes;
+  final List<InfoBox> restaurantInfoBoxes;
 
   @override
   CustomTopBarState createState() => CustomTopBarState();
@@ -15,81 +23,73 @@ class CustomTopBar extends StatefulWidget {
 
 class CustomTopBarState extends State<CustomTopBar> {
   int selectedIndex = 0;
+  late List<InfoBox> currentInfoBoxes;
 
-  List<TopMenuModel> topMenuList = [
-    TopMenuModel(
-      text: "Recipes",
-      type: TopBarEnum.Recipes,
-    ),
-    TopMenuModel(
-      text: "Restaurant",
-      type: TopBarEnum.Restaurant,
-    ),
-  ];
-
-  // Info boxes for Recipes
-  List<InfoBox> recipesInfoBoxes = [
-    InfoBox(title: 'Recipe Info 1', description: 'Recipe Description 1'),
-    InfoBox(title: 'Recipe Info 2', description: 'Recipe Description 2'),
-    InfoBox(title: 'Recipe Info 3', description: 'Recipe Description 3'),
-    InfoBox(title: 'Recipe Info 4', description: 'Recipe Description 4'),
-    // ... Add more later
-  ];
-
-  // Info boxes for Restaurant
-  List<InfoBox> restaurantInfoBoxes = [
-    InfoBox(
-        title: 'Restaurant Info 1', description: 'Restaurant Description 1'),
-    InfoBox(
-        title: 'Restaurant Info 2', description: 'Restaurant Description 2'),
-    // ... Add more later
-  ];
+  @override
+  void initState() {
+    super.initState();
+    // Set the initial InfoBoxes
+    currentInfoBoxes = widget.recipeInfoBoxes;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: List.generate(topMenuList.length, (index) {
-            return InkWell(
-              onTap: () {
-                selectedIndex = index;
-                widget.onChanged?.call(topMenuList[index].type);
-                setState(() {});
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 50.0),
-                child: Text(
-                  topMenuList[index].text,
-                  style: TextStyle(
-                    color: selectedIndex == index
-                        ? const Color.fromRGBO(233, 30, 99, 1)
-                        : appTheme.gray500,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    decoration: selectedIndex == index
-                        ? TextDecoration.underline
-                        : TextDecoration.none,
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 50,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: widget.menuItems.length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    selectedIndex = index;
+                    widget.onChanged(widget.menuItems[index].type);
+                    setState(() {
+                      // Switch InfoBoxes based on the selected menu item
+                      currentInfoBoxes =
+                          (widget.menuItems[index].type == TopBarEnum.Item1)
+                              ? widget.recipeInfoBoxes
+                              : widget.restaurantInfoBoxes;
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 60.0),
+                    child: Text(
+                      widget.menuItems[index].text,
+                      style: TextStyle(
+                        color: selectedIndex == index
+                            ? const Color.fromRGBO(233, 30, 99, 1)
+                            : Colors.grey,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        decoration: selectedIndex == index
+                            ? TextDecoration.underline
+                            : TextDecoration.none,
+                        decorationColor: selectedIndex == index
+                            ? const Color.fromRGBO(233, 30, 99, 1)
+                            : Colors.transparent,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            );
-          }),
-        ),
-        SizedBox(height: 16.0),
-        if (topMenuList[selectedIndex].type == TopBarEnum.Recipes)
-          InfoBoxGrid(recipesInfoBoxes),
-        if (topMenuList[selectedIndex].type == TopBarEnum.Restaurant)
-          InfoBoxGrid(restaurantInfoBoxes),
-      ],
+                );
+              },
+            ),
+          ),
+          SizedBox(height: 16.0),
+          InfoBoxGrid(currentInfoBoxes), // Use the current InfoBoxes
+        ],
+      ),
     );
   }
 }
 
 enum TopBarEnum {
-  Recipes,
-  Restaurant,
+  Item1,
+  Item2,
 }
 
 class TopMenuModel {
@@ -99,6 +99,5 @@ class TopMenuModel {
   });
 
   String text;
-
   TopBarEnum type;
 }
